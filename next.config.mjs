4a25ef18c017bcb493cf4619992b3b5ce8b7d3bc
ved35 <110726699+ved35@users.noreply.github.com>
2024-03-29 08:45:52 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output : "export",
     images:{
         remotePatterns : [
             {
@@ -8,7 +7,8 @@ const nextConfig = {
                 hostname : 'images.pexels.com',
             }
         ]
-    }
+    },
+    output : 'export',
 };
 
 export default nextConfig;
